29e032bf843e04ad42ae7313f64baa9b34b09a1a
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2023-10-14 21:47:09 +0300

    Adding entry point and routes

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express()
+import routes from './routes/routes';
+
+app.use(express.json())
+
+// prefix route api/v1
+const routeBase = '/api/v1';
+
+app.use(routeBase, routes);
+
+export default app;
